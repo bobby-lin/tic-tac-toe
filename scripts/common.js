77b457e0e0c6bc927c7ea096a45dcb44e6b80540
Bobby Lin <bobbylinjh@gmail.com>
2015-12-29 17:42:08 +0800

    Added user selection menu into common.js for #4

--- scripts/common.js
+++ scripts/common.js
@@ -2,3 +2,15 @@
  * Created on: 29/12/15
  *     Author: Bobby Lin
  */
+
+function startGame() {
+    $('#menu').addClass('.hidden');
+}
+
+function start(choice) {
+    console.log(choice);
+    setTimeout(function(){
+        $('#select-menu').remove();
+        $('#page-mask').remove();
+    }, 200);
+}
